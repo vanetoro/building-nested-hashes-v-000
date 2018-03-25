19848c33e1987cfd40152d16bdbbe9daa4b5aa48
Vanessa Toro <vanetoro84@gmail.com>
2018-03-25 16:37:52 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,17 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        {"Benvolio" =>
+          :name => 'Benvolio',
+          :age => '17',
+          :attitude => 'worried'
+          
+        },
+          { 'Mercutio' =>
+            :name => 'Mercutio',
+            :age => '18',
+            :atitute => 'hot-headed'
+        
       ]
    }, 
    :capulet => {
