da2ac4a896e5ed44eae6a25acc0f680b48ce6804
Vanessa Toro <vanetoro84@gmail.com>
2018-03-25 16:24:49 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
       :hero_friends => {}
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriach => {},
+      :hero => {},
+      :hero_friends => {}
       }}
 
   
